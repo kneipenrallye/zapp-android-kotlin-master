8a444fef1c93e7f7a21473d45badc94668f24671
kage <kage1>
2020-07-27 23:08:08 +0200

    fix news bug

--- app/src/main/java/com/example/othregensburg/zapp/MainActivity.kt
+++ app/src/main/java/com/example/othregensburg/zapp/MainActivity.kt
@@ -2,11 +2,10 @@ package com.example.othregensburg.zapp
 
 import android.content.ContextWrapper
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.pixplicity.easyprefs.library.Prefs
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.btn_generate
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +25,10 @@ class MainActivity : AppCompatActivity() {
         // Save context for push notification
         NewsActivity.newsContext = this.applicationContext
 
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) { }
+
         btn_livemap.setOnClickListener {
             val intent = Intent (this, MapsActivity::class.java).apply {  }
             startActivity(intent)
@@ -37,26 +40,16 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent);
         }
 
-        btn_notification.setOnClickListener {
-            val intent = Intent(this, NewsActivity::class.java).apply {
-                putExtra(NewsActivity.TITLE_EXTRA, "Push Notification Title")
-                putExtra(NewsActivity.DESCRIPTION_EXTRA, "Push Notification Description")
-            }
-            startActivity(intent);
-        }
-
         btn_registration.setOnClickListener {
             val intent = Intent(this, RegistrationActivity::class.java).apply { }
             startActivity(intent);
         }
 
-        btn_account.setOnClickListener {
-            val intent = Intent(this, AccountActivity::class.java).apply { }
-            startActivity(intent);
-        }
-
         btn_news.setOnClickListener {
-            val intent = Intent(this, NewsActivity::class.java).apply { }
+            val intent = Intent(this, NewsActivity::class.java).apply {
+//                putExtra(NewsActivity.TITLE_EXTRA, "Push Notification Title")
+//                putExtra(NewsActivity.DESCRIPTION_EXTRA, "Push Notification Description")
+            }
             startActivity(intent);
         }
 
@@ -78,11 +71,5 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        btn_generate.setOnClickListener {
-
-            Intent(this, QRCodeGenerator::class.java).apply {
-                startActivity(this)
-            }
-        }
     }
 }
